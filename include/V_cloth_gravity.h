e76e31725d6a301e62440297bc986cb73f543db3
David Levin <dilevin@csail.mit.edu>
2019-10-23 11:38:56 -0400

    remove cloth gravity potential

--- include/V_cloth_gravity.h
+++
@@ -1,8 +0,0 @@
-#include <Eigen/Dense>
-#include <EigenTypes.h>
-
-//  M - sparse mass matrix for the entire mesh
-//  g - the acceleration due to gravity
-//Output:
-//  V - potential energy due to gravity for the entire mesh
-void V_cloth_gravity(double &V, Eigen::SparseMatrixd &M, Eigen::Ref<const Eigen::Vector3d> g);
--- src/V_cloth_gravity.cpp
+++
@@ -1,5 +0,0 @@
-#include <V_cloth_gravity.h>
-
-void V_cloth_gravity(double &V, Eigen::SparseMatrixd &M, Eigen::Ref<const Eigen::Vector3d> g) {
-
-}
